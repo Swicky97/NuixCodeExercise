80b20d8744af0762ed25599464c0b4890869f25a
Chase Swick <chaseswick@gmail.com>
2025-05-13 19:48:21 -0400

    Add Swagger UI for easier testing

--- InvestmentPerformanceApi/Program.cs
+++ InvestmentPerformanceApi/Program.cs
@@ -1,22 +1,26 @@
 using InvestmentPerformanceApi.Repos;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Investment Performance API", Version = "v1" });
+});
 
 builder.Services.AddSingleton<InvestmentRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Investment Performance API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 //app.UseHttpsRedirection();
 
